646e446ebef405f5b53321f190fdf3abe05afa2b
Volodymyr Berezhnyi <volodymyr.berezhnyi@gmail.com>
2025-11-12 20:20:23 +0300

    Added backend skeleton and test API routes

---
+++ frontend/src/layouts/MainLayout.vue
@@ -0,0 +1,16 @@
+
+<template>
+  <div class="flex flex-col min-h-screen">
+    <HeaderLayout />
+    <main class="flex-grow pt-20 bg-gray-50">
+      <RouterView />
+    </main>
+    <FooterLayout />
+  </div>
+</template>
+
+<script setup>
+import { RouterView } from 'vue-router'
+import FooterLayout from '@/components/layout/FooterLayout.vue'
+import HeaderLayout from '@/components/layout/HeaderLayout.vue';
+</script>
